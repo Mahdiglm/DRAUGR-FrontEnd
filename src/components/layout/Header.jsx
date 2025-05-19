637bf7dad52097febcf5a21a7e9116d3af7c40b9
Mahdiglm <Mahdiglm@users.noreply.github.com>
2025-05-19 07:07:49 -0700

    کاهش جزئی فاصله بین عناصر هدر

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -174,7 +174,7 @@ const Header = ({ cartItems, onCartClick }) => {
           </motion.button>
 
           {/* Spacer div to force distance */}
-          <div className="w-6 sm:w-8 md:w-12"></div>
+          <div className="w-5 sm:w-6 md:w-10"></div>
 
           <motion.div 
             whileHover={{ scale: 1.1, rotate: [0, -5, 5, 0] }}
@@ -198,7 +198,7 @@ const Header = ({ cartItems, onCartClick }) => {
           </motion.div>
           
           {/* Another spacer div */}
-          <div className="w-4 sm:w-6 md:w-8"></div>
+          <div className="w-3 sm:w-5 md:w-6"></div>
           
           {/* Mobile Menu Button - Smaller for phones */}
           <motion.button 
