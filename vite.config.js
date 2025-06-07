3080d05a17caaf330555a38a85efc96a8eff9443
Mahdiglm <Mahdiglm@users.noreply.github.com>
2025-06-07 22:07:15 +0330

    افزودن تنظیمات سرور با پراکسی برای درخواست‌های API

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/DRAUGR-FrontEnd/' : '/',
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   build: {
     assetsDir: 'assets',
     assetsInlineLimit: 4096, // 4kb - files smaller than this will be inlined as base64
